7ea04b5c642ab4f41968700cd24a3be3779fd594
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-11-05 16:17:33 +0530

    update folder structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Navbar from "./Nav.jsx";
+import Navbar from "./components/Nav.jsx";
 import "./index.css";
-import Footer from "./Footer.jsx";
+import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
